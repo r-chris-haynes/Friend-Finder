d03ae80d993a144d4a6ba776fe466b00d5be1db2
chris haynes <chrishaynes@chriss-mbp.home>
2019-06-13 10:45:21 -0400

    server listening

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,9 @@
+// var friendsData = require("../data/friendsData");
+
+// module.exports = function(app) {
+ 
+//   app.get("/api/friends", function(req, res) {
+//       res.json(friendsData);
+//     });
+// }
+   
