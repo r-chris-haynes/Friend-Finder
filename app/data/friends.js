20832cd29141c5acba4de6f0369d0f1c88cfc824
chris haynes <chrishaynes@chriss-MacBook-Pro.local>
2019-06-13 21:24:06 -0400

    added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,71 @@
-var friendArray = [];
+var friendArray = [
+    {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "choices":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+  {
+    "name":"Angie",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        3,
+        5,
+        2,
+        2,
+        1,
+        2,
+        3,
+        4,
+        4,
+        2
+      ]
+  },
+  {
+    "name":"Anna",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        1,
+        5,
+        2,
+        3,
+        4,
+        5,
+        5,
+        5,
+        1,
+        2
+      ]
+  },
+  {
+    "name":"Alex",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        2,
+        4,
+        1,
+        5,
+        5,
+        5,
+        1,
+        4,
+        5,
+        3
+      ]
+  }
+  
+  
+  
+];
 
 module.exports = friendArray;
